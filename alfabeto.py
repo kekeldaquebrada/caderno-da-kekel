72723c6feb50dad851c1abd2a674a7f7fb3c17e4
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-05-09 15:07:43 -0300

    Add files via upload

---
+++ alfabeto.py
@@ -0,0 +1,14 @@
+letrinha= input ("Digite uma letra:")
+print("Voce digitou", letrinha)
+if letrinha=="a":
+    print ("Voce digitou uma vogal")
+elif letrinha=="e":
+    print("voce digitou uma vogal")
+elif letrinha=="i":
+    print ("Voce digitou uma vogal ")
+elif letrinha=="o":
+     print ("Voce digitou uma vogal ")
+elif letrinha=="u":
+     print ("Voce digitou uma vogal ")
+else:
+    print ("Voce digitou uma consoante")
